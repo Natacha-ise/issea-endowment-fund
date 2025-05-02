3c7f5afd07801cce1a1a1c7fc9f2d7d40aa0114e
Natacha-ise <fortunempolah@gmail.com>
2025-05-02 10:18:16 +0200

    Delete courbe_taux-main directory

--- courbe_taux-main/courbe_taux/Datas/data_link.py
+++
@@ -1,5 +0,0 @@
-import os# utiliser pour le chemin d'accès
-
-def data_dir(data_name):
-    main=os.path.dirname(__file__)
-    return  os.path.join(main, data_name)
--- courbe_taux-main/courbe_taux/Photos/photo_link.py
+++
@@ -1,5 +0,0 @@
-import os# utiliser pour le chemin d'accès
-
-def main_dir(script_path):
-    main=os.path.dirname(__file__)
-    return  os.path.join(main, script_path)
